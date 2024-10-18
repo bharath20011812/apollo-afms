4488309f391bf424c802750fa43b7107d461ef42
Bharathkumar <85354718+bharath20011812@users.noreply.github.com>
2024-10-18 13:53:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -207,3 +207,32 @@ $('.moreless-button').click(function () {
     $(this).text("Read less")
   }
 });
+
+
+
+
+
+$(window).scroll(function() {
+ $(".anm_mod").each(function() {
+  const position = $(this).offset().top;
+  const scroll = $(window).scrollTop();
+  const windowHeight = $(window).height();
+  if (scroll > position - windowHeight) {
+   $(this).addClass("active");
+  }
+  if (scroll < 100) {
+   $(this).removeClass("active");
+  }
+ });
+});
+
+$(function() {
+ $('a[href^="#"]').click(function() {
+  const speed = 800;
+  const href = $(this).attr("href");
+  const target = $(href == "#" || href == "" ? "html" : href);
+  const position = target.offset().top;
+  $("html, body").animate({ scrollTop: position }, speed, "swing");
+  return false;
+ });
+});
